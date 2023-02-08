56b3ff9e79ec6c984651d7e94e62dbf43896d965
nawar-b-tek <bedouinour20@gmail.com>
2023-02-08 15:10:51 +0100

    bootstrap

--- university/student/models.py
+++ university/student/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+class Matiere(models.Model):
+    libelle= models.CharField(max_length=20)
+    note_ds = models.FloatField()
+    note_ex = models.FloatField()
+
 
 class Classes (models.Model):
     libelle = models.CharField(max_length=20)
+    matiere= models.ManyToManyField(Matiere)
 
     
 class Etudiant (models.Model):
