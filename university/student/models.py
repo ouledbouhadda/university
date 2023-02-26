a13dfcddcf33674401aa9f3e1b220802e4ab5894
Ben-Ammar1312 <ben.ammar1312@gmail.com>
2023-02-26 11:07:46 +0100

    login

--- university/student/models.py
+++ university/student/models.py
@@ -1,28 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-class Matiere(models.Model):
-    libelle= models.CharField(max_length=20)
-    note_ds = models.FloatField()
-    note_ex = models.FloatField()
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+class Subject(models.Model):
+    libelle = models.CharField(max_length=20)
     def __str__(self):
         return self.libelle
+
+class Classes(models.Model):
+    libelle = models.CharField(max_length=20)
+    matiere = models.ManyToManyField(Subject)
+    def __str__(self):
+        return self.libelle 
+
+class Student(models.Model):
+    student_profile = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete= models.CASCADE,related_name='+')
+    student_classes=models.ForeignKey(Classes,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.student_profile.username
+
+class Student_Subject (models.Model):
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    student= models.ForeignKey(Student,on_delete=models.CASCADE)
+    note_ds = models.DecimalField(max_digits=4,decimal_places=2,null=True)
+    note_ex = models.DecimalField(max_digits=4,decimal_places=2,null=True)
     def moy(self):
         return self.note_ds * 0.4 + self.note_ex * 0.6
-    
-
-
-class Classes (models.Model):
-    libelle = models.CharField(max_length=20)
-    matiere= models.ManyToManyField(Matiere)
     def __str__(self):
-        return self.libelle
+        return f'{self.student} -> {self.subject}'
     
-class Etudiant (models.Model):
-    nom = models.CharField(max_length=20)
-    prenom= models.CharField(max_length=20)
-    age = models.IntegerField()
-    classes=models.ForeignKey(Classes,on_delete=models.CASCADE)
+class Teacher(models.Model):
+    profile = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete= models.CASCADE,related_name='+')
+    teacher_subject=models.ManyToManyField(Subject)
+
     def __str__(self):
-        return self.nom
-    
-    
+        return self.profile.username
