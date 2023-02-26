4299ef36e899bbe6ed76efdfab67f0f7ca8c2479
Ben-Ammar1312 <ben.ammar1312@gmail.com>
2023-02-26 21:28:12 +0100

    logout

--- university/student/urls.py
+++ university/student/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 from . import views
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='home'),
     path('student/', views.student_home, name='student_home'),
     path('teacher/', views.teacher_home, name='teacher_home'),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
